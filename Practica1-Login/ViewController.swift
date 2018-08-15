6e434f90726724d327a6fbace615252a06ffc5d4
USUARIO <usuario@F102-23.local>
2018-08-14 20:55:59 -0500

    Initial Commit

---
+++ Practica1-Login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practica1-Login
+//
+//  Created by I104-21 on 14/08/18.
+//  Copyright © 2018 I104-21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Practica1-LoginTests/Practica1_LoginTests.swift
@@ -0,0 +1,36 @@
+//
+//  Practica1_LoginTests.swift
+//  Practica1-LoginTests
+//
+//  Created by I104-21 on 14/08/18.
+//  Copyright © 2018 I104-21. All rights reserved.
+//
+
+import XCTest
+@testable import Practica1_Login
+
+class Practica1_LoginTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
